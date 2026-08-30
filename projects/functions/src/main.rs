5de43fd6be8f1cd677703b6d053acf4572b59313
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2026-08-30 13:07:32 -0300

    Agrego parte funcional respecto a funciones invisibles, declaraciones y expresiones

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
+    // Funciones
     println!("Hello, world!");
     otra_funcion();
+
+    // Funciones con parametros
     funcion_con_parametros(5);
     imprimir_medidas_y_unidad(5, 'h');
+
+    // Funcional (declaraciones y expresiones)
+    let _y = {
+        let x = 3;
+        x // retorna x
+    }; // Es lo mismo que hacer 'let y = (let x = 3)', pero rust no permite ese tipo de declaraciones, pues no retornan valores, la manera correcta es crear una función sin nombre con {} y hacer que retorne algún valor para que pueda asignarsele a la variable. En este caso el valor x se pierde luego de salir de la llave porque se usó dentro del contexto de esta función invisible, ver "Understanding Ownership (Cap-4)".
+    // En mi opinión: Esto es un fenómeno del paradigma funcional puro y duro, hacer simplemente y = x = 3 no es una opción viable dadas las reglas de Rust de no retornar dentro de declaraciones, solo de expresiones. Esto tiene sus complicaciones pero traen por detrás un montón de ventajas a la hora de manejar memoria.
+
+    println!("El valor de y es: {y}"); // 3
+
+    // Funciones con retorno
+
 }
 
 fn otra_funcion() {
