b4deaacfe3098c426dd548679af989ebd7356f6f
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2026-08-30 13:12:34 -0300

    Termino parte funciones Cap-3

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     imprimir_medidas_y_unidad(5, 'h');
 
     // Funcional (declaraciones y expresiones)
-    let _y = {
+    let y = {
         let x = 3;
         x // retorna x
     }; // Es lo mismo que hacer 'let y = (let x = 3)', pero rust no permite ese tipo de declaraciones, pues no retornan valores, la manera correcta es crear una función sin nombre con {} y hacer que retorne algún valor para que pueda asignarsele a la variable. En este caso el valor x se pierde luego de salir de la llave porque se usó dentro del contexto de esta función invisible, ver "Understanding Ownership (Cap-4)".
@@ -17,7 +17,11 @@ fn main() {
     println!("El valor de y es: {y}"); // 3
 
     // Funciones con retorno
+    let x = cinco();
+    funcion_con_parametros(x); // 5
 
+    let x = valor_mas_uno(5);
+    funcion_con_parametros(x); // 6 
 }
 
 fn otra_funcion() {
@@ -30,4 +34,12 @@ fn funcion_con_parametros(x: i32) {
 
 fn imprimir_medidas_y_unidad(medida: i32, unidad: char) {
     println!("El valor medido es: {medida}{unidad}");
+}
+
+fn cinco() -> i32 {
+    5
+}
+
+fn valor_mas_uno(valor: i32) -> i32 {
+    valor + 1
 }
