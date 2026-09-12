c063a120a4c8ddf5b0bfb9b515db7f0550bdc2df
juanPedroBasualdo <jpbasualdo@fi.uba.ar>
2026-09-12 16:19:09 -0300

    Termino seccion strings de collections

--- projects/colections/src/main.rs
+++ projects/colections/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     // Vectores
+    
     let _v: Vec<i32> = Vec::new();
     let _v = vec![1, 2, 3]; // Tipo inferido por el valor que contiene.
 
@@ -46,5 +47,67 @@ fn main() {
 
         // Hacer cosas con v
     } // <- v sale de scope y por lo tanto se dropea junto con sus elementos.
+
+    // Strings
+
+    let _s = String::from("Hola, mundo");
+    let _s = "Hello world".to_string(); // El to_string() hace que un string literal pase a un String collection.
+
+    // Actualizando un string
+    let mut s = "Hola".to_string();
+    s.push_str(" mundo");
+    println!("{s}");
+
+    let mut s1 = "Hola".to_string();
+    let s2 = " mundo";
+    s1.push_str(s2);
+    println!("{s1}");
+    println!("s2 es: '{s2}'"); // push_str() no toma ownership del contenido de s2.
+    let mut s = "lo".to_string();
+    s.push('l');
+    println!("{s}");
+
+    let s1 = "Hola ".to_string();
+    let s2 = "mundo".to_string();
+    let s = s1 + &s2;
+    println!("{s}");
+    println!("{s2}"); // add() no toma posesión de s2 pero si de s1
+
+    let s1 = "Tic".to_string();
+    let s2 = "Tac".to_string();
+    let s3 = "Toe".to_string();
+    let _s = s1 + "-" + &s2 + "-" + &s3; //Esto hace Tic-Tac-Toe
+
+    let s1 = "Tic".to_string();
+    let s = format!("{s1}-{s2}-{s3}"); // Esto tambien hace Tic-Tac-Toe pero no se queda con s1.
+    println!("s1: {s1}, s2: {s2}, s3: {s3}, s: {s}");
+
+    // Indexar a un String
+    let hello = "hola".to_string();
+    let answer = hello.chars().nth(0); // Rust guarda los strings como UTF-8, como UTF-8 guarda en distintos tamaños sus caracteres, indexar un string es un poco más complicado que simplemente hacer &hello[0]. Se puede usar .chars() para dividir el string en chars o .bytes() para obtener el valor en bytes del caracter. Luego .nth(n) devuelve el valor en el n-avo lugar del string como un Option<byte> o Option<char> según si se usó .chars() o .bytes().
+
+    match answer {
+        Some(answer) => println!("{answer}"),
+        _ => (),
+    }; // Esto al final va a imprimir 'h'
+
+    // Indexando slices
+    let hello = "Hola";
+    let s = &hello[0..1]; // Los slices si se pueden indexar, pero debe ser en un intervalo.
+    println!("{s}"); // 'H'
+
+    // Metodo para iterar sobre Strings
+    let hello = "hola".to_string();
+
+    for c in hello.chars() {
+        println!("{c}");
+    } // .chars() devuelve un tipo de dato tipo Chars<'_> que es iterable y puede ser impreso por stdout sobrepasando la necesidad de usar matching.
+
+    for c in "hola".bytes() {
+        println!("{c}");
+    } // .bytes() devuelve un tipo de dato tipo Bytes<'_> que tiene las mismas características que Chars<'_>.
+
+    
+
 
 }
